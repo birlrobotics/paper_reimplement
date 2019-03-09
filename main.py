1c33e847d43afd642603c255c8a00739560e7783
huangjiancong1 <huangjiancong863@gmail.com>
2019-03-09 20:23:29 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import numpy as np
-from sklearn.cluster import DBSCAN
-import ipdb; ipdb.set_trace()
+from dbscan_bhatt import DBSCAN
 X = np.array([[1, 2], [2, 2], [2, 3], [8, 7], [8, 8], [25, 80]])
-clustering = DBSCAN(eps=3, min_samples=2).fit(X)
-clustering2 = DBSCAN(eps=1, min_samples=3).fit(X)
+clustering = DBSCAN(X, 3, 2).fit(X)

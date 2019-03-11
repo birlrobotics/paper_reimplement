835be05cd43b1d77eff3aed97d90751a2e0aed9e
YijiongLin <87113273@qq.com>
2019-03-11 21:48:37 +0800

    framework v0.02

--- Recovery_Implement/region.py
+++ Recovery_Implement/region.py
@@ -1,61 +1,79 @@
 # finished half, cluster, DBSCAN, B_distance should be done
 # follow the data type of learn_state region function
-
+from copy import copy
 #  Arguments:
 #       experience_list is a list of namedtuples: [(s,z,a,r,s',z'),(s,z,a,r,s',z'),...]
 #       action_list is list of action space, generally from demo_record
 #  Return:
 #       a set of regions , each region is a set of bunch of experience tuples.
-class Region_Cluster(experience_list,action_list):
+class Region_Cluster():
 
     def __init__(self):
 
+        self.e_list = []
+        self.a_dict = {}
+        self.region_phi_result_set = set()
+
+# Refer to Algorithm 1: Learning State Distribution Components
+# phi_set is the lower case phi, region_phi_set is the capital case phi(which always has the prefix region_).
+# Psi_set is also this case
+"""Cluter a bunch of input points(states).
+
+Parameters
+----------
+experience_list:(list)
+    a list of experience namedtuples.
+
+action_dict:(action_dict)
+    a dict of demonstration actions.
+Return
+------
+return_set : (set)
+	a set of regions, the result of clusterring and merging.
+    (psi_set, psi_set, ...)
+    psi_set = set((s,z,a,r,s',z'),...)
+"""
+    def learn_state_region(self,experience_list,action_dict):
         self.e_list = experience_list
-        self.a_list = action_list
-        self.region_phy_result_set = set()
-#       Refer to Algorithm 1: Learning State Distribution Components
-# phy_set is the lower case phy, region_phy_set is the capital case phy(which always has the prefix region_).
-    def learn_state_region(self):
-        merged_region_phy_set = set()
-        for act in self.a_list:
-            rigon_phy_hat_set = set()
-            same_act_exp_set = self.extract_exp_with_same_component('action',act)
-            region_psi_set = self.cluster( same_act_exp_set ,distance_type = 'states_dist',component='states')
+        self.a_dict = action_dict
+        # line 2
+        for act_index in self.a_dict.keys():
+            # line 3
+
+            rigon_phi_hat_set = set()
+
+            same_act_exp_set = self.extract_exp_with_same_action(act_index)
+            region_psi_set = self.cluster(same_act_exp_set ,distance_type = 'states_dist',component='next_states')
             # convert set to list for iteration
             list_region_psi_set = list(region_psi_set)
             # psi_set is a set of experience namedtuples within one output region psi,
             for psi_set in list_region_psi_set:
-                region_phy_hat_hat_set = self.cluster(psi_set,distance_type = 'states_dist',component='next_states')
+                region_phi_hat_hat_set = self.cluster(psi_set,distance_type = 'states_dist',component='states')
                 # Union set operation
-                region_phy_hat_set |= region_phy_hat_hat_set
+                region_phi_hat_set |= region_phi_hat_hat_set
+            # Line 11-12
             # P is a set of capital regions
-            P = self.cluster(region_phy_hat_set,distance_type = 'states_dist',component='next_states')
-            # Merge those phy_set in same region_phy_set
-            for P_region_phy_set in P:
-                merged_phy_set = set()
-                for each_phy_set in P_region_phy_set:
-                    merged_phy_set |= each_phy_set
-                merged_region_phy_set.add(merged_phy_set)
+            P = self.cluster(region_phi_hat_set,distance_type = 'region_dist', component= null)
+            # Merge those phi_set in same region_phi_set
+            merged_region_phi_set = set()
+            for P_region_phi_set in P:
+                merged_phi_set = set()
+                for each_phi_set in P_region_phi_set:
+                    merged_phi_set |= each_phi_set
+                merged_region_phi_set.add(merged_phi_set)
+            self.region_phi_result_set |= merged_region_phi_set
 
-            self.region_phy_result_set |= merged_region_phy_set
-
-        return_set = self.region_phy_result_set
+        return_set = self.region_phi_result_set
         return  return_set
 
-#     Get a set of experience namedtuple with same component: [(s,z,a,r,s',z'),...]
-    def extract_exp_with_same_component(self,type,component):
-        if type == 'action':
-            same_component_exp_set = set()
-            for e in self.e_list:
-                if e.a == component:
-                    same_component_exp_set.add(e)
-
-        elif type == 'next_state':
-            same_component_exp_set = set()
-            for e in self.e_list:
-                if e.next_s == component:
-                    same_component_exp_set.add(e)
-
+#     Get a set of experience namedtuple with same action: [(s,z,a,r,s',z'),...]
+    def extract_exp_with_same_action(self,act_index):
+        same_component_exp_set = set()
+        for e in self.e_list:
+            # each exp tuple just have one action, so donnot need to iterate, just take the first one
+            action_list = list(e.action.keys())
+            if action_list[0] == act_index:
+                same_component_exp_set.add(e)
         return same_component_exp_set
 
 
@@ -75,6 +93,7 @@ class Region_Cluster(experience_list,action_list):
         TODO
 
  # DBSCAN algorithm
+
     def DBSCAN(self, distance_type):
         TODO
 

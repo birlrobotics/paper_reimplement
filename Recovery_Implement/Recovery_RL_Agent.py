4ddaad10cefa6e9d41aed4dadbf3df5259666f79
huangjiancong1 <huangjiancong863@gmail.com>
2019-04-06 14:20:24 +0800

    update learning funnels input_set

--- Recovery_Implement/Recovery_RL_Agent.py
+++ Recovery_Implement/Recovery_RL_Agent.py
@@ -19,41 +19,55 @@ import random
 import numpy as np
 # import gaussian_tool as gt
 from buffer import Exp_Buffer
-from region import Region_Cluster
+# from region import Region_Cluster
 from collections import OrderedDict
+from qmodel import Value_Function
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+LR = 5e-4               # learning rate 
+BATCH_SIZE = 3
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cpu")
 class Agent():
 
-    def __init__(self, seed = 0):
+    def __init__(self, seed = 0, dim =2 ):
 
         """
-        self.demo_act = {a_i: goal_postion}
+        self.demo_act_dict = {a_1: goal_position, ... , a_i: goal_position,... }
+            a_i(int), goal_position(array)
+
+        self.regions_dict = {r_1:(mean,cov), ... , r_i:(mean,cov), ...}
+            r_i(int), mean(array), cov(ndarray)
+
         """
         self.demo_act_dict = OrderedDict()
-        self.num_of_demo_goal = OrderedDict()
-#         self.state_value_func = Value_Function()
-        self.exps_list = Exp_Buffer()
-        self.cluster = Region_Cluster()
+        self.num_of_demo_goal = 0
+        self.demo_goal = []
+        self.q_func = Value_Function()
+        self.memory = Exp_Buffer(batch_size= BATCH_SIZE)
+
+        self.regions_dict = OrderedDict()
+        # The dimension of the workspace (2 or 3).
+        self.dim = dim
+#         self.cluster = Region_Cluster()
 
         """
         self.act for new skills
         """
         
         self.actions_dict = OrderedDict()
-        self.state_size = 0
+        self.state_size = dim
         self.action_size = 0
         self.seed = random.seed(seed)
 
 
-        """
-        self.region_dict:(dictionary)
-        {region_index: phi_set},
-        region_index = i
-        phi_set = set((s,z,a,r,next_s,next_z),...)
-        """
-        self.regions_infs_list=[]
-        self.region_dict={}
-        self.num_of_region = 0
-
+        self.funnels_infs_list=[]
+        
+        self.funnels_amount= 0
+        self.separate_s_c_exp_list = []
 
     def demo_record(self,goal_tuples):
         """To record a task demonstration skill with several goal states,
@@ -63,11 +77,15 @@ class Agent():
         ----------
         goal_tuples:(2 dim array)
         """
+        self.demo_goal = goal_tuples
+        self.demo_amount = len(goal_tuples)
         for i, goal in enumerate(goal_tuples):
             action_index = str(i)
             self.demo_act_dict[action_index] = goal
-        demo_act_dict = self.demo_act_dict
-        return demo_act_dict
+            
+
+        print("Agent: agent has recorded the demonstration as its original skills: {} \n".format(self.demo_act_dict))
+        return self.demo_act_dict
 
     def get_demo_act_dict(self):
         """Return the action dictionary of demonstration
@@ -77,8 +95,8 @@ class Agent():
             keys: index string
             values: goal position
         """
-        demo_act_dict = self.demo_act_dict
-        return demo_act_dict
+        self.demo_act_dict
+        return self.demo_act_dict
 
     def exp_record(self,episode_list):
         """Record experience tuples of an episode to experience list
@@ -88,8 +106,19 @@ class Agent():
             a list of experience tuple.
         """
         for exp_tuple in episode_list:
-            state, contact, action, reward, next_state, next_contact = exp_tuple
-            self.exps_list.add(state, contact, action, reward, next_state, next_contact, False)
+            state, action, reward, next_state, is_goal = exp_tuple
+            self.memory.add(state, action, reward, next_state, is_goal)
+    
+    def separate_exp_record(self,episode_list):
+        """Record experience tuples of an episode to experience list
+        Param
+        ----------
+        episode_list:(list)
+            a list of experience tuple.
+        """
+        for exp_tuple in episode_list:
+            state,contact,  action, reward, next_state,  next_contact, done = exp_tuple
+            self.memory.separate_add( state,contact,  action, reward, next_state,  next_contact, done)
 
     # return a list of namedtuple for experience.
     def get_exp_list(self):
@@ -99,69 +128,27 @@ class Agent():
         experiences_list:(list)
             a list of experience tuple.
         """
-        experiences_list = self.exps_list.get_experience_list()
+        experiences_list = self.memory.get_experience_list()
         return experiences_list
 
-
-
-    def learn_cluster_region(self):
-        """Call the cluster method to get regions. And assign the relative action to each region.
-        Also determine a region whether it is a goal region.
-
-        region_phi_result_set:(set)
-            a set of regions, the result of clusterring and merging.
-            (psi_set, psi_set, ...)
-            psi_set = set((s,z,a,r,s',z'),...)
-
-        regions_infs_list = [phi_1, phi_2, ...]
-            phi_i = [phi_set, act_dict]
-            phi_set = ((state,contact,action,reward,next_state,next_contact),...)
-            act_dict = [a_index: goal_postion]
+    def get_separate_exp_list(self):
+        """Return the list of all the restored experience tuples
+        Return
+        ----------
+        experiences_list:(list)
+            a list of experience tuple.
         """
-        pass
-        exp_tuples_list = self.get_exp_list()
-        region_phi_result_set = self.cluster.learn_state_region(exp_tuples_list, self.demo_act_dict)
-        for i, phi_set in enumerate(region_phi_result_set):
-            phi_list = phi_set
-            self.regions_infs_list.append([])
-            self.regions_infs_list[i].append(deepcopy(phi_set))
-            # phi_list.[0].a is  {a_index:goal_position}
-            self.regions_infs_list[i].append(deepcopy(phi_list[0].action))
-            # determine a region if it is goal region, by checking its action whether is empty or not.
-            if bool(phi_list[0].action) == False:
-                self.regions_infs_list[i].append(True)
-            else:
-                self.regions_infs_list[i].append(False)
-
-        self.num_of_region = len(region_phi_result_set)
+        return self.memory.get_separate_exp_list()
 
 
-    def get_region_number(self):
-        """Get the number of regions.
+    def get_funnels_amount(self):
+        """Get the amount of funnels.
         Return
         ------
-        region_number:(int)
+        self.funnels_amount(int):
         """
         pass
-        region_number = self.num_of_region
-        return region_number
-
-
-    def gaussian_likelihood(self):
-        """Call the gaussian maximum likelihood estimation method.
-        Put mean and std in each region_infs_list's phi_i 3rd and 4th index of place.
-        regions_infs_list = [phi_1, phi_2, ...]
-            phi_i = [phi_set, act_dict] ->  phi_i = [phi_set, act_dict, mean, std]
-                phi_set = ((state,contact,action,reward,next_state,next_contact),...)
-                act_dict = [a_index: goal_postion]
-                mean = array()
-                std = array()
-        """
-        pass
-        for i, region_infs_list in enumerate(self.regions_infs_list):
-            mean, std = gt.gaussian_estimation(region_infs_list[0])
-            self.regions_infs_list[i].append(mean)
-            self.regions_infs_list[i].append(std)
+        return self.funnels_amount
 
 
 
@@ -169,38 +156,60 @@ class Agent():
         """Construct region directed graph used for algorithm 2.
         """
         pass
-    def init_value_function(self):
-        """Initialization of the approximate value function with the number of region.
+
+
+    def test_generate_funnels_infs_list(self):
+        cov = np.eye(self.dim * 3)
+        regions = []
+        origional_point =  np.zeros(self.dim)
+        regions.append(origional_point)
+        for each_goal in self.demo_goal:
+            regions.append(each_goal)
+
+        self.funnels_amount = len(regions)
+
+        funnels_infs_list=[]
+        for region_position, (key,value) in zip(regions,self.demo_act_dict.items()):
+            alist = []
+            act_dict = {}
+            act_dict[key] = value
+            alist.append(key)
+            alist.append(act_dict)
+
+            # Generate the mean covering the position and contact
+            contact_mean = self.test_generate_contact_mean(region_position)
+            mean = np.append(region_position, contact_mean)
+
+            region_dict = {}
+            region_dict[key] = (mean,cov)
+
+            alist.append(region_dict)
+
+            funnels_infs_list.append(alist)
+        print("Agent: test funnels infs:{} \n".format(funnels_infs_list))
+        return funnels_infs_list
+            
+    def test_generate_contact_mean(self, position):
+        return np.zeros(self.dim*2)
+
+    def test_init_value_function(self,funnels_infs_list,init_Q_value):
+        """Initialization of the approximate value function with the amount of funnels.
         """
-        pass
-        self.state_value_func.init_region_number(self.num_of_region, self.demo_act_dict)
+        self.funnels_amount = len(funnels_infs_list)
+        self.funnels_infs_list = funnels_infs_list
+        # Initialize the q function with th funnels information.
+        self.q_func.init_funnels_inf(self.funnels_amount, self.demo_act_dict, funnels_infs_list,init_Q_value )
 
-    def learn_initial_policy(self):
-        """Learn the policy with the experience tuple.
+    def test_learn_initial_policy(self):
+        """Learn the policy with the original experience tuple.
         """
-        pass
-        # get a tuple of ndarrays: (ndarray, ndarray, ...)
-        experiences_batch = self.exps_list.sample()
-        states, contacts, actions, rewards, next_states, next_contacts, dones = experiences
-        # Get max predicted Q values (for next states) from target model
-        # max(1)[0] take the maximum value along a given axis=1 (row).
-        # Because it do not need to compute the gradient of target, use detach.
-        Q_targets_next = self.state_value_func_target(next_states).detach().max(1)[0].unsqueeze(1)
-        # Compute Q targets for current states
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        states,actions,rewards,next_states,dones = self.memory.batch_sample()
+        loss = self.q_func.q_learn(states,actions,rewards,next_states,dones)
+        return loss
+    
+    def get_funnels_q_value(self):
+        return self.q_func.get_funnels_q()
 
-        # Get expected Q values from local model, along row to get the index with actions
-        Q_expected = self.state_value_func_local(states).gather(1, actions)
-
-        # Compute loss
-        loss = F.mse_loss(Q_expected, Q_targets)
-        # Minimize the loss
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
-        # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
 
         #  not done
     def soft_update(self, local_model, target_model, tau):
@@ -223,11 +232,7 @@ class Agent():
 
         Return
         ------
-        self.region_prob_list:(list)
-        [region_prob_tuple], region_prob_tuple = (region_index,mean,std,is_goal)
+
         """
         pass
-    def learn_Q_value(self, ):
-        """Update Q function with experience tuple
-        """
-        pass
+
--- Recovery_Implement/test_main.py
+++ Recovery_Implement/test_main.py
@@ -1,18 +1,68 @@
+from __future__ import print_function
 import Recovery_RL_Agent
 import env_robot
+from torch.autograd import Variable
 import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from scipy.stats import norm
+from scipy.stats import multivariate_normal
+from math import sqrt
+import random
+from math import log
+import matplotlib.pyplot as plt
+
+def training(agent):
+        epoches = 100
+        max_t = 1000
+        training_loss=[]
+        for i_epoch in range(epoches):
+                total_loss = 0 
+                for t in range(max_t):
+                        loss = agent.test_learn_initial_policy()
+                        total_loss += loss
+                        print('\rEpisode {}\tloss: {:.5f}'.format(i_epoch, loss),end = "")
+                training_loss.append(total_loss)
+                print('\rEpisode {}\tloss: {:.5f}'.format(i_epoch, loss))
+        print('\n',agent.get_funnels_q_value())
+        return training_loss
 
 robot = env_robot.Env(dim=2)
-agent = Recovery_RL_Agent.Agent()
+agent = Recovery_RL_Agent.Agent(dim=2)
 
-goal_array = np.array(([100,100],[100,300],[300,300]))
-demo_act_dict = agent.demo_record(goal_array)
+demo_states_list =  [[100,50],[200,200],[300,400],[400,350],[450,450],[500,550]]
+robot.demonstration(demo_states_list)
+demo_act_dict = agent.demo_record(demo_states_list)
 
 repeat_times = 100
 
 for i in range(0,repeat_times):
     # executing the demo action and restore experience tuples in agent
-    episode_record = robot.execute_demo_act(demo_act_dict)
+    episode_record, separate_s_c_record = robot.execute_separate_demo_act(demo_act_dict)
+    # episode_record = robot.execute_demo_act(demo_act_dict)
+    
     agent.exp_record(episode_record)
+    # agent.separate_exp_record(separate_s_c_record)
     # Reset env, back to start point
     robot.test_reset()
+# print(agent.get_exp_list())
+funnels_infs_list = agent.test_generate_funnels_infs_list()
+
+agent.test_init_value_function(funnels_infs_list,init_Q_value = -1200)
+training_loss = training(agent)
+agent.test_init_value_function(funnels_infs_list,init_Q_value = (-np.inf))
+inf_training_loss = training(agent)
+
+
+figsize = 15,9
+figure, ax = plt.subplots(figsize=figsize)
+
+
+plt.plot(np.arange(1, len(training_loss)+1), training_loss, color='orangered' )
+plt.plot(np.arange(1, len(inf_training_loss)+1), inf_training_loss, color='blue' )
+
+plt.ylabel('training_loss')
+plt.xlabel('Episode #')
+plt.savefig('training_loss_comparation_1200&inf.png')
+plt.show()

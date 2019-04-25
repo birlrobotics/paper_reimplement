88c0f102909b2bfe61d94eb2b8a9f285a9adf362
huangjiancong1 <huangjiancong863@gmail.com>
2019-04-25 14:51:36 +0800

    tsne.npy

--- plot_tsne.py
+++ plot_tsne.py
@@ -25,18 +25,28 @@ colors = {3:'black',
 print(kitting.shape)
 
 tsne = TSNE(n_components=3, learning_rate=100).fit_transform(kitting)
+a = tsne
+np.save("tsne.npy", a)
 
-# plt.scatter(tsne[:, 0], tsne[:, 1], c=colors[t], label ='Skill ' + str(1), alpha=0.5)
-# plt.scatter(tsne[:, 0], tsne[:, 1], c=tag, label ='Skill ' + str(1), alpha=0.5)
-fig = plt.figure()
-ax = fig.gca(projection='3d', adjustable='box')
 
-for tg, p in zip(tag, tsne):
-    ax.scatter(p[0], p[1], p[2], c=colors[tg], label ='Skill ' + str(tg), alpha=0.55)
+# fig = plt.figure()
 
-handles, labels = plt.gca().get_legend_handles_labels()
-by_label = OrderedDict(zip(labels, handles))
-plt.legend(by_label.values(), by_label.keys(), loc=1)
-fig.savefig('tsne.png', format="png", dpi=300)
-# plt.colorbar()
-plt.show()
+
+# ax = fig.gca(projection='3d', adjustable='box')
+
+# # plt.scatter(tsne[:, 0], tsne[:, 1], c=colors[t], label ='Skill ' + str(1), alpha=0.5)
+# # plt.scatter(tsne[:, 0], tsne[:, 1], c=tag, label ='Skill ' + str(1), alpha=0.5)
+# ax = ax.scatter(tsne[:, 0], tsne[:, 1], tsne[:, 2], c=tag*20, label ='Skill ' + str(tag), alpha=0.5)
+
+
+# plt.legend()
+
+# # for tg, p in zip(tag, tsne):
+# #     ax.scatter(p[0], p[1], p[2], c=colors[tg], label ='Skill ' + str(tg), alpha=0.55)
+
+# # handles, labels = plt.gca().get_legend_handles_labels()
+# # by_label = OrderedDict(zip(labels, handles))
+# # plt.legend(by_label.values(), by_label.keys(), loc=1)
+# # fig.savefig('tsne.png', format="png", dpi=300)
+# # plt.colorbar()
+# plt.show()

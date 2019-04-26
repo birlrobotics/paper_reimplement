6ff8297d5255b50796ee6119cf24a6652befd8bd
huangjiancong1 <huangjiancong1@gmail.com>
2019-04-26 11:35:35 +0800

    first generate 20 tsne with infomation of to comparison

--- plot_tsne.py
+++ plot_tsne.py
@@ -27,35 +27,55 @@ print(kitting.shape)
 
 start = time.clock()
 
-# tsne = TSNE(n_components=3, learning_rate=100).fit_transform(kitting)
-# a = tsne
-tsne = np.load('/home/jim/Recovery_reimplement/tsne.npy')
+# for i in range(20):
+#     tsne = TSNE(n_components=2, learning_rate=100).fit_transform(kitting)
+#     a = tsne
+
+#     np.save("tsne"+str(i), a)
+
+#     # # plt.scatter(tsne[:, 0], tsne[:, 1], c=colors[t], label ='Skill ' + str(1), alpha=0.5)
+#     plt.scatter(tsne[:, 0], tsne[:, 1], c=tag, label ='Skill ' + str(1), alpha=0.5)
+
+#     # fig = plt.figure()
+#     # ax = fig.gca(projection='3d', adjustable='box')
+#     # ax = ax.scatter(tsne[:, 0], tsne[:, 1], tsne[:, 2], c=tag*20, label ='Skill ' + str(tag), alpha=0.5)
+
+#     # # for tg, p in zip(tag, tsne):
+#     # #     ax.scatter(p[0], p[1], p[2], c=colors[tg], label ='Skill ' + str(tg), alpha=0.55)
+
+#     # # handles, labels = plt.gca().get_legend_handles_labels()
+#     # # by_label = OrderedDict(zip(labels, handles))
+#     # # plt.legend(by_label.values(), by_label.keys(), loc=1)
+#     # # fig.savefig('tsne.png', format="png", dpi=300)
+#     # # plt.colorbar()
+#     plt.legend()
+#     # plt.show()
+#     plt.savefig('tsne'+str(i), format="png", dpi=300)
+
+
+tsne = np.load('/home/jim/Recovery_reimplement/tsne0.npy')
 # tsne = np.load('/home/jim/Recovery_reimplement/tsne_499.npy')
 
-# np.save("tsne_499.npy", a)
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d', adjustable='box')
-
 # # plt.scatter(tsne[:, 0], tsne[:, 1], c=colors[t], label ='Skill ' + str(1), alpha=0.5)
-# # plt.scatter(tsne[:, 0], tsne[:, 1], c=tag, label ='Skill ' + str(1), alpha=0.5)
-ax = ax.scatter(tsne[:, 0], tsne[:, 1], tsne[:, 2], c=tag*20, label ='Skill ' + str(tag), alpha=0.5)
+# plt.scatter(tsne[:, 0], tsne[:, 1], c=tag, label ='Skill ' + str(1), alpha=0.5)
 
+# fig = plt.figure()
+# ax = fig.gca(projection='3d', adjustable='box')
+# ax = ax.scatter(tsne[:, 0], tsne[:, 1], tsne[:, 2], c=tag*20, label ='Skill ' + str(tag), alpha=0.5)
 
-plt.legend()
+for tg, p in zip(tag, tsne):
+    plt.scatter(p[0], p[1], c=colors[tg], label ='Skill ' + str(tg), alpha=0.55)
 
-# # for tg, p in zip(tag, tsne):
-# #     ax.scatter(p[0], p[1], p[2], c=colors[tg], label ='Skill ' + str(tg), alpha=0.55)
-
-# # handles, labels = plt.gca().get_legend_handles_labels()
-# # by_label = OrderedDict(zip(labels, handles))
-# # plt.legend(by_label.values(), by_label.keys(), loc=1)
+handles, labels = plt.gca().get_legend_handles_labels()
+by_label = OrderedDict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys(), loc=1)
 # # fig.savefig('tsne.png', format="png", dpi=300)
 # # plt.colorbar()
+plt.legend()
 plt.show()
+# plt.savefig('tsne'+str(i), format="png", dpi=300)
 
-#long running
-#do something other
+
+
 end = time.clock()
 print end-start 
